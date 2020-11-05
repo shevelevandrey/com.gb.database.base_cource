a2f6ffdec147569d684b9bc3419bc58a8055299a
shevelevandrey <shevelevandrey@mail.ru>
2020-11-05 19:39:48 +0300

    Doing lesson #3.

--- query/employees.sql
+++ query/employees.sql
@@ -1,12 +1,12 @@
 # 1. Выбрать среднюю зарплату по отделам.
-select dep.dept_name, avg(sal.salary)
+select dep.dept_no, dep.dept_name, avg(sal.salary)
   from employees.employees emp
   join employees.salaries sal on emp.emp_no = sal.emp_no
   join employees.dept_emp de on de.emp_no = emp.emp_no
   join employees.departments dep on dep.dept_no = de.dept_no
 where sal.from_date <= curdate()
   and sal.to_date > curdate()
-group by dep.dept_name;
+group by dep.dept_no, dep.dept_name;
 
 # 2. Выбрать максимальную зарплату у сотрудника.
 select emp.emp_no, emp.first_name, emp.last_name, dep.dept_name, max(sal.salary)
